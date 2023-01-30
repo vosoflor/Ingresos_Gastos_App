1b21e3f2b64db92ce51b4f6c046722e9776418ff
Verónica Osorio Florián <vero_oso11@hotmail.com>
2023-01-29 20:56:07 -0500

    Cambios templates delete y utilización Jinja

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -2,11 +2,23 @@ from app_registro import app
 from flask import render_template
 
 @app.route("/")
-
 def index():
-    return render_template("index.html")
+   
+   # Prueba de diccionario a vista html
+    datos = [{ 'Fecha' : '18/12/2022', 'Concepto' : 'Regalo de reyes', 'Cantidad' : -275.50},
+            { 'Fecha' : '19/12/2022', 'Concepto' : 'Cobro de trabajo', 'Cantidad' : 1200.00},
+            { 'Fecha' : '18/12/2022', 'Concepto' : 'Ropa de navidad', 'Cantidad' : -355.50},]
+
+    return render_template("index.html", pageTitle = "Lista", lista = datos)
 
 @app.route("/new")
-
 def create():
-    return render_template("new.html")
+    return render_template("new.html", pageTitle = "Nuevo registro")
+
+@app.route("/modify")
+def modify():
+    return render_template("update.html", pageTitle = "Modificar registro")
+
+@app.route("/delete")
+def delete():
+    return render_template("delete.html", pageTitle = "Eliminar registro")
