de288d8098bb1c3d4ee2d8f2075bcafb35f98f2d
Verónica Osorio Florián <vero_oso11@hotmail.com>
2023-02-04 13:37:55 -0500

    Incluir validación de datos en formulario, completar ruta de borrado y ajustar fichero de datos con ID

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -5,34 +5,97 @@ import csv
 
 @app.route("/")
 def index():
-    fichero = open("data/movimientos.csv", "r") # Llama al archivo
-    csvReader = csv.reader(fichero, delimiter = ",", quotechar = '"') # Accede a cada registro del archivo y lo formatea
-    datos = [] # Array de datos vacío para cargar los registros del archivo
-
-    for item in csvReader: # Recorrer el objeto csvReader y cargar cada registro al array
-        datos.append(item)
-
+    datos = listItems("data/movimientos.csv")
     return render_template("index.html", pageTitle = "Lista", lista = datos)
 
 @app.route("/new", methods = ["GET", "POST"])
 def create():
     if request.method == "GET":
-        return render_template("new.html", pageTitle = "Nuevo registro", typeAction = "un nuevo registro", buttonAction = "Guardar")
+        return render_template("new.html", pageTitle = "Nuevo registro", typeAction = "un nuevo registro", buttonAction = "Guardar", requestForm = {})
     else:
-        dateForm = datetime.strptime(request.form["Fecha"], '%Y-%m-%d').date()
-        if(dateForm <= date.today()):
+        error = validateForm(request.form)
+        if not error:
+            # Asigna ID
+            datos = listItems("data/last_id.csv")
+            if not datos:
+                id = 1
+            else:
+                id = int(datos[0][0]) + 1
+            
+            # Actualiza último ID
+            last_id = open("data/last_id.csv", "w")
+            last_id.write(str(id))
+            last_id.close()
+
+            # Agrega nuevo registro
             mifichero = open("data/movimientos.csv", "a", newline = '') # Acceder al archivo y configurarlo para cargar un nuevo registro
             lectura = csv.writer(mifichero, delimiter = ',', quotechar = '"') # Con método de escritura se define formato para csv
-            lectura.writerow([request.form["Fecha"], request.form["Concepto"], request.form["Cantidad"]]) # Registramos los datos recibidos desde el formulario con request.form y lo añadimos con el método writerrow
+            lectura.writerow([id, request.form["Fecha"], request.form["Concepto"], request.form["Cantidad"]]) # Registramos los datos recibidos desde el formulario con request.form y lo añadimos con el método writerrow
             mifichero.close()
+            # Direcciona a URL de inicio (index)
             return redirect("/")
         else:
-            return redirect("/new")
+            return render_template("new.html", pageTitle = "Nuevo registro", typeAction = "un nuevo registro", buttonAction = "Guardar", msgError = error, requestForm = request.form)
 
-@app.route("/modify")
-def modify():
-    return render_template("update.html", pageTitle = "Modificar registro", typeAction = "una modificación", buttonAction = "Modificar")
+@app.route("/modify/<int:id>", methods = ["GET", "POST"])
+def modify(id):
+    if request.method == "GET":
+        registroBuscado = lookUpItem(id)
+        if registroBuscado:
+            return render_template("update.html", pageTitle = "Modificar registro", typeAction = "una modificación", buttonAction = "Modificar", requestForm = request.form)
 
-@app.route("/delete")
-def delete():
-    return render_template("delete.html", pageTitle = "Eliminar registro")
+@app.route("/delete/<int:id>", methods = ["GET", "POST"])
+def delete(id):
+    if request.method == "GET":
+        registroBuscado = lookUpItem(id)
+        if registroBuscado:
+            return render_template("delete.html", pageTitle = "Eliminar registro", registro = registroBuscado, buttonAction = "Eliminar")
+        else:
+            return redirect("/")
+    else:
+        oldFile = listItems("data/movimientos.csv")
+        newFile = oldFile
+
+        for itemPosition, item in enumerate(newFile):
+            if int(item[0]) == id:
+                newFile.pop(itemPosition)
+                break
+
+        with open("data/movimientos.csv", "w") as file:
+            mywriter = csv.writer(file, delimiter = ",")
+            mywriter.writerows(newFile)
+
+        return redirect("/")
+
+# Función para hacer validación de datos del formulario
+def validateForm(requestForm):
+    error = []
+    requestDate = datetime.strptime(requestForm["Fecha"], '%Y-%m-%d').date()
+    if requestForm["Fecha"] == "" or requestDate > date.today():
+        error.append("Fecha inválida: La fecha introducida es en el futuro o está vacía")
+    if requestForm["Concepto"] == "":
+        error.append("Concepto vacío: Introduce una descripción")
+    if requestForm["Cantidad"] == "" or float(requestForm["Cantidad"]) == 0.0:
+        error.append("Cantidad vacío o cero: Debes introducir un monto válido")
+    return error
+ 
+# Función para devolver lista de registros en fichero de datos
+def listItems(ruta):
+    fichero = open(ruta, "r") # Llama al archivo
+    csvReader = csv.reader(fichero, delimiter = ",", quotechar = '"') # Accede a cada registro del archivo y lo formatea
+    datos = [] # Array de datos vacío para cargar los registros del archivo
+
+    for item in csvReader: # Recorrer el objeto csvReader y cargar cada registro al array
+        datos.append(item)
+    
+    return datos
+
+# Función para devolver registro específico en fichero de datos
+def lookUpItem(id):
+    datos = listItems("data/movimientos.csv")
+    for item in datos:
+        if int(item[0]) == id:
+            return item
+        else:
+            return []
+
--- ejemplos.py
+++ ejemplos.py
@@ -1,13 +1,49 @@
 # Lectura y escritura de archivos
 import csv
-
+from app_registro import routes
 '''
 with open("data/movimientos.csv", "r") as resultado:
     leer = resultado.readline()
     print(leer)
-'''
+
 
 mifichero = open("data/movimientos.csv", "a", newline = '')
 lectura = csv.writer(mifichero, delimiter = ',', quotechar = '"')
 lectura.writerow(['19/12/2022', 'Compra de árbol de navidad', -100.50])
 mifichero.close()
+
+mifichero = open("data/movimientos.csv", "r") # Acceder al archivo y configurarlo para cargar un nuevo registro
+csvReader = csv.reader(mifichero, delimiter = ",", quotechar = '"') # Accede a cada registro del archivo y lo formatea
+datos = [] # Array de datos vacío para cargar los registros del archivo
+
+for item in csvReader: # Recorrer el objeto csvReader y cargar cada registro al array
+    datos.append(item)
+
+print(len(datos))
+print(type(datos))
+print(datos[len(datos)-1])
+print(datos[len(datos)-1][0])
+print(type(datos[len(datos)-1][0]))
+print(int(datos[len(datos)-1][0]))
+print(type(int(datos[len(datos)-1][0])))
+
+datos = routes.listItems("data/last_id.csv")
+print(datos)
+print(type(datos))
+print(int(datos[0][0]) + 1)
+
+from app_registro import routes
+
+oldFile = routes.listItems("data/movimientos_prueba.csv")
+newFile = oldFile
+
+for itemPosition, item in enumerate(newFile):
+    if int(item[0]) == 1:
+        newFile.pop(itemPosition)
+        break
+
+with open("data/movimientos_prueba.csv", "w") as file:
+    mywriter = csv.writer(file, delimiter = ",")
+    mywriter.writerows(newFile)
+
+'''
